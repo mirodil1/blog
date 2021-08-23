41b0d39b680b99df3a805096c024b4ba24447d21
Mirodil <ortikov.m97@gmail.com>
2021-08-23 11:40:17 +0500

    Project added

---
+++ mysite/blog/views.py
@@ -0,0 +1,107 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentPostForm, SearchForm
+from taggit.models import Tag
+from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+# Create your views here.
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 5)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts,
+                   'tag': tag
+                   })
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status = 'published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentPostForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentPostForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
+
+def post_search(request):
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        query = request.GET['query']
+
+        search_vector = SearchVector('title', 'body')
+        search_query = SearchQuery(query)
+        results = Post.published.annotate(
+            search = search_vector,
+            rank = SearchRank(search_vector, search_query)
+        ).filter(search=search_query).order_by('-rank')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'query': query,
+                   'results': results})
+
+# def resume(request):
+#     return render(request,
+#                   'resume_template/resume.html')
